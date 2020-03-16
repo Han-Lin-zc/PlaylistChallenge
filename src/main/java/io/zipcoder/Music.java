45b434ebf0008a45e3dcf339859be8633e6ec323
hlin <Zxsaqw12@&$>
2020-03-16 19:59:43 -0400

    Work in progress

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,57 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Music {
 
-    private String[] playList;
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
 
-    public Music(String[] playList){
-        this.playList = playList;
+
+    public Music(String name, String artist){
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<Song>();
     }
 
-    public Integer selection(Integer startIndex, String selection){
+    public boolean addSong(String title, double duration) {
+        if (findSong(title) == null) {
+            this.songs.add(new Song(title,duration));
+            return true;
+        }
+        return false;
+    }
+
+    private Song findSong(String title) {
+        for (Song checkedSong : this.songs) {
+            if (checkedSong.getTitle().equals(title)) {
+                return checkedSong;
+            }
+        }
         return null;
     }
+
+    public boolean addToPlaylist(int trackNumber, LinkedList<Song> playlist) {
+        int index = trackNumber - 1;
+        if ((index >= 0) && (index <= this.songs.size())) {
+            playlist.add(this.songs.get(index));
+            return true;
+        }
+        System.out.println("This album does not have a track" + trackNumber);
+        return false;
+    }
+
+
+    public boolean addToPlaylist(String title, LinkedList<Song> playlist) {
+        Song checkedSong = findSong(title);
+        if (checkedSong != null) {
+            playlist.add(checkedSong);
+            return true;
+        }
+        System.out.println("The song " + title + " is not in this album");
+        return false;
+    }
+
 }
